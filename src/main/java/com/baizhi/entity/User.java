ed1116fc8e207e424334df8a236d971779548dfb
zhujx <2055828139@qq.com>
2020-07-18 03:34:48 +0800

    yingx-zjx2055828139第一个版本提交 V1.0

---
+++ src/main/java/com/baizhi/entity/User.java
@@ -0,0 +1,71 @@
+package com.baizhi.entity;
+
+public class User {
+    private String id;
+    private String username;
+    private String password;
+    private String addressId;
+    private String age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(String addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public User(String id, String username, String password, String addressId, String age) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.addressId = addressId;
+        this.age = age;
+    }
+
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", addressId='" + addressId + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
